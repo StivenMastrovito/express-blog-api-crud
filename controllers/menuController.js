ce088595a0d0bc42103d4800823d121a6a0a7326
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-12-18 14:30:25 +0100

    feat: bonus

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,34 +1,65 @@
 import menu from "../dati.js"
 
-function index(req, res){
-    res.send("Mostra tutto il menu");
+function index(req, res) {
+    const tag = req.query.tags;
+    let filteredMenu = menu;
+
+    if (tag !== undefined) {
+        filteredMenu = menu.filter(({ tags }) => tags.includes(tag));
+    }
+
+    res.json(filteredMenu)
+
+
 }
 
-function show(req, res){
+function show(req, res) {
     const id = parseInt(req.params.id);
-    res.send(`mostra l'elemento con id: ${id}`);
+    const elem = menu.find((item) => item.id === id);
+
+    if (elem === undefined) {
+        res.status(404);
+        return res.json({
+            error: "Not Found",
+            message: "Elemento non trovato",
+        })
+    }
+    res.json(elem)
 }
 
-function store(req, res){
+function store(req, res) {
     res.send("Aggiungi un elemento");
 }
 
-function update(req, res){
+function update(req, res) {
     const id = parseInt(req.params.id);
 
     res.send(`Modifica l'elemento con id: ${id}`);
 }
 
-function modify(req, res){
+function modify(req, res) {
     const id = parseInt(req.params.id);
 
     res.send(`Modifica parzialmente l'elemento con id: ${id}`);
 }
 
-function destroy(req, res){
+function destroy(req, res) {
     const id = parseInt(req.params.id);
+    console.log(id);
 
-    res.send(`Elimino l'elemento con id ${id}`);
+    const indexId = menu.findIndex((item) => item.id === id);
+    console.log(indexId);
+
+    if (indexId === -1) {
+        res.status(404);
+        return res.json({
+            error: "Not Found",
+            message: "Elemento non trovato",
+        })
+    }
+
+    menu.splice(indexId, 1);
+    res.status(200);
 }
 const controller = {
     index,
--- dati.js
+++ dati.js
@@ -3,85 +3,96 @@ const menu = [
         id: 1,
         nome: "Espresso",
         prezzo: 1,
-        lista: "caffetteria"
+        tags: ["caffetteria"]
     },
     {
         id: 2,
         nome: "Cappuccino",
         prezzo: 1.50,
-        lista: "caffetteria"
+        tags: ["caffetteria"]
+
     },
     {
         id: 3,
         nome: "Latte Macchiato",
         prezzo: 2,
-        lista: "caffetteria"
+        tags: ["caffetteria"]
+
     },
     {
         id: 4,
         nome: "Succo di frutta",
         prezzo: 2,
-        lista: "caffetteria"
+        tags: ["caffetteria"]
+
     },
     {
         id: 5,
         nome: "Cornetto vuoto",
         prezzo: 1.30,
-        lista: "caffetteria"
+        tags: ["caffetteria"]
+
     },
     {
         id: 6,
         nome: "Cornetto farcito",
         prezzo: 1.50,
-        lista: "caffetteria"
+        tags: ["caffetteria"]
+
     },
     {
         id: 7,
         nome: "Caffe Leccese",
         prezzo: 2,
-        lista: "caffetteria"
+        tags: ["caffetteria"]
+
     },
     {
         id: 8,
         nome: "Acqua",
         prezzo: 0.7,
-        lista: "bar",
+        tags: ["bar"],
     },
     {
         id: 9,
         nome: "Coca-cola",
         prezzo: 2.00,
-        lista: "bar",
+        tags: ["bar"],
+
     },
     {
         id: 10,
         nome: "Fanta",
         prezzo: 2,
-        lista: "bar",
+        tags: ["bar"],
+
     },
     {
         id: 11,
         nome: "Estathe",
         prezzo: 2,
-        lista: "bar",
+        tags: ["bar"],
+
     },
     {
         id: 12,
         nome: "Spritz",
         prezzo: 2,
-        lista: "bar",
+        tags: ["bar"],
+
     },
     {
         id: 13,
         nome: "Red-Bull",
         prezzo: 2.50,
-        lista: "bar",
+        tags: ["bar"],
+
     },
     {
         id: 14,
         nome: "Schweppes",
         prezzo: 2,
-        lista: "bar",
+        tags: ["bar"],
     },
 
 ];
